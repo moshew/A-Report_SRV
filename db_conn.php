40ff9a54e334de58aa624c7e2b80b17acbe6b225
moshew <moshe.waisman@gmail.com>
2017-06-08 22:27:39 +0300

    Create db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,5 @@
 <?php
-   $conn = new mysqli('127.0.0.1', 'db_usr', '1234', 'report_db');
+   $conn = new mysqli('127.0.0.1', 'xxx', 'xxx', 'report_db');
    $conn->query("SET NAMES 'utf8'");
    $conn->query("SET CHARACTER SET utf8");
 
